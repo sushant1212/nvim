e0e207b2ec9e0e709ea05057c73885ffe9ad9b01
Sushant S <sushantswamy1212@gmail.com>
2026-02-21 12:06:32 +0100

    update

--- lua/sushant/init.lua
+++ lua/sushant/init.lua
@@ -80,3 +80,19 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.splitbelow = true  -- Put new windows below current
 vim.opt.splitright = true  -- Put new windows right of current
 
+-- Diagnostic Config & Keymaps
+-- See :help vim.diagnostic.Opts
+vim.diagnostic.config {
+    update_in_insert = false,
+    severity_sort = true,
+    float = { border = 'rounded', source = 'if_many' },
+    underline = { severity = vim.diagnostic.severity.ERROR },
+
+    -- Can switch between these as you prefer
+    virtual_text = true, -- Text shows up at the end of the line
+    virtual_lines = false, -- Text shows up underneath the line, with virtual lines
+
+    -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
+    jump = { float = true },
+}
+
