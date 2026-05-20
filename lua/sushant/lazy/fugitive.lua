e63c2321c84ee3035243e221867971231eeb166c
Sushant S <sushantswamy1212@gmail.com>
2026-05-20 14:52:01 +0200

    open git status in new tab

--- lua/sushant/lazy/fugitive.lua
+++ lua/sushant/lazy/fugitive.lua
@@ -1,7 +1,7 @@
 return {
     "tpope/vim-fugitive",
     config = function()
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        vim.keymap.set("n", "<leader>gs", "<esc><cmd>tabnew<CR><cmd>Git<CR>")
 
         local sushant_Fugitive = vim.api.nvim_create_augroup("sushant_Fugitive", {clear = true})
 
