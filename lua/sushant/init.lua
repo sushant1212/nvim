1ced5854759f66e2f3aca10c632ead4270fc562a
Sushant S <sushantswamy1212@gmail.com>
2026-08-28 16:14:08 +0200

    undo prev commit

--- lua/sushant/init.lua
+++ lua/sushant/init.lua
@@ -65,7 +65,6 @@ function EnableCleanSave()
 end
 
 -- Setup vim.g values
-vim.g.mapleader = " "
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
--- lua/sushant/remap.lua
+++ lua/sushant/remap.lua
@@ -1,5 +1,4 @@
-vim.g.mapleader = " "
-
+vim.g.mapleader = " "  -- leader needs to be set before loading lazy
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
