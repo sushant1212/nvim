03ddbaa16252d9721d5a4643e82ce70d0b02a75b
Sushant S <sushantswamy1212@gmail.com>
2026-09-03 09:35:27 +0200

    finally fix tab issues, customize blink settings

--- lua/sushant/lazy/blink.lua
+++ lua/sushant/lazy/blink.lua
@@ -8,29 +8,7 @@ return {
     version = '1.*',
     dependencies = {
         -- Snippet Engine
-        {
-            'L3MON4D3/LuaSnip',
-            version = '2.*',
-            build = (function()
-                -- Build Step is needed for regex support in snippets.
-                -- This step is not supported in many windows environments.
-                -- Remove the below condition to re-enable on windows.
-                if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then return end
-                return 'make install_jsregexp'
-            end)(),
-            dependencies = {
-                -- `friendly-snippets` contains a variety of premade snippets.
-                --    See the README about individual language/framework/plugin snippets:
-                --    https://github.com/rafamadriz/friendly-snippets
-                -- {
-                --   'rafamadriz/friendly-snippets',
-                --   config = function()
-                --     require('luasnip.loaders.from_vscode').lazy_load()
-                --   end,
-                -- },
-            },
-            opts = {},
-        },
+        'L3MON4D3/LuaSnip',  -- loaded inside snippets.lua
     },
 
     opts = {
@@ -56,8 +34,20 @@ return {
             -- <c-k>: Toggle signature help
             --
             -- See :h blink-cmp-config-keymap for defining your own keymap
-            preset = 'default',
+            preset = 'none',
+            ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+            ['<C-e>'] = { 'hide', 'fallback' },
+            ['<C-y>'] = { 'select_and_accept', 'fallback' },
 
+            ['<Up>'] = { 'select_prev', 'fallback' },
+            ['<Down>'] = { 'select_next', 'fallback' },
+            ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+            ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+
+            ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+            ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+            ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
             -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
             --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
         },
@@ -72,10 +62,20 @@ return {
             -- By default, you may press `<c-space>` to show the documentation.
             -- Optionally, set `auto_show = true` to show the documentation after a delay.
             documentation = { auto_show = true, auto_show_delay_ms = 100 },
+            list = { selection = { preselect = false, auto_insert = true } },
+            menu = {
+                draw = {
+                    columns = {
+                        { "kind_icon", "kind" },
+                        { "label", "label_description", gap = 1 },
+                    },
+                    treesitter = { "lsp" }
+                }
+            },
         },
 
         sources = {
-            default = { 'lsp', 'path', 'snippets' },
+            default = { 'lsp', 'path', 'snippets', 'buffer' },
         },
 
         snippets = { preset = 'luasnip' },
@@ -87,7 +87,7 @@ return {
         -- the rust implementation via `'prefer_rust_with_warning'`
         --
         -- See :h blink-cmp-config-fuzzy for more information
-        fuzzy = { implementation = 'lua' },
+        fuzzy = { implementation = "prefer_rust_with_warning" },
 
         -- Shows a signature help window while you type arguments for a function
         signature = { enabled = true },
--- lua/sushant/lazy/snippets.lua
+++ lua/sushant/lazy/snippets.lua
@@ -1,4 +1,3 @@
-
 return {
     {
         "L3MON4D3/LuaSnip",
@@ -12,19 +11,21 @@ return {
         config = function()
             local ls = require("luasnip")
             ls.filetype_extend("javascript", { "jsdoc" })
+            vim.keymap.set({ "i" }, "<C-K>", function()
+                ls.expand()
+            end, { silent = true })
+            vim.keymap.set({ "i", "s" }, "<C-J>", function()
+                ls.jump(1)
+            end, { silent = true })
+            vim.keymap.set({ "i", "s" }, "<C-L>", function()
+                ls.jump(-1)
+            end, { silent = true })
 
-            --- TODO: What is expand?
-            vim.keymap.set({"i"}, "<C-s>e", function() ls.expand() end, {silent = true})
-
-            vim.keymap.set({"i", "s"}, "<C-s>;", function() ls.jump(1) end, {silent = true})
-            vim.keymap.set({"i", "s"}, "<C-s>,", function() ls.jump(-1) end, {silent = true})
-
-            vim.keymap.set({"i", "s"}, "<C-E>", function()
+            vim.keymap.set({ "i", "s" }, "<C-E>", function()
                 if ls.choice_active() then
                     ls.change_choice(1)
                 end
-            end, {silent = true})
+            end)
         end,
-    }
+    },
 }
-
